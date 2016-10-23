1a1967d4d3a0ef1fbab1f2792f7cc2b13b901941
SauliusB <saulius.bucka@gmail.com>
2016-10-23 13:25:57 +0300

    fixed bug with https, pressing back

--- chrome_plugin/scripts/eventPage.js
+++ chrome_plugin/scripts/eventPage.js
@@ -1,6 +1,5 @@
-﻿// alert("eventPage loadina!");
-console.log("Vykdomas eventPage.js")
-var tukstReceptuUrl = "http://www.delfi.lt/1000receptu/receptai/";
+﻿console.log("Vykdomas eventPage.js")
+const MATCH_URL = "http://tomasra.com:5000/match";
 function checkWebpage(loadingStatus) {
     if (loadingStatus === undefined) {
         // No url yet or no changes in it
@@ -8,54 +7,47 @@ function checkWebpage(loadingStatus) {
     else if (loadingStatus === 'loading') {
         chrome.storage.local.remove('ingredientuSarasas');
         // Check if page contains a recipe and store result in local storage
-        chrome.tabs.query({active: true}, function(tab) {
+        chrome.tabs.query({ active: true }, function (tab) {
             tab = tab[0];
             var fullUrl = tab.url;
             console.log(fullUrl);
             var recipeSiteExtractor = GetRecipeExtractor(fullUrl);
             if (recipeSiteExtractor) {
-                // console.log('recipe found');
                 chrome.storage.local.set({ recipeFound: true });
                 chrome.browserAction.setBadgeText({ text: "!" });
                 chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-active.png" });
             } else {
-                // console.log('recipe not found');
                 chrome.storage.local.set({ recipeFound: false });
                 chrome.browserAction.setBadgeText({ text: "" });
                 chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-neutral.png" });
             }
         });
     } else if (loadingStatus === 'complete') {
-        chrome.tabs.query({active: true}, function(tab) {
+        chrome.tabs.query({ active: true }, function (tab) {
             tab = tab[0];
-            chrome.tabs.sendMessage(tab.id, "getDOM", {}, function (response){
+            chrome.tabs.sendMessage(tab.id, "getDOM", {}, function (response) {
                 if (response && response.jsonObjektas) {
                     var jsonObjektas = response.jsonObjektas;
-                    // console.log(jsonObjektas);
                     var jsonString = JSON.stringify(jsonObjektas);
-                    // console.log(jsonString);
+
                     $.ajax({
                         type: "post",
-                        url: "http://tomasra.com:5000/match",
+                        url: MATCH_URL,
                         data: jsonString,
                         dataType: "json",
                         contentType: "application/json; charset=utf-8",
-                        success: function (data){
-                            // console.log(data);
-                            //Gavus duomenis, cia galime deti i chrome localStorage:
+                        success: function (data) {
                             chrome.storage.local.set({
-                                ingredientuSarasas : data,
+                                ingredientuSarasas: data,
                                 recipeTitle: response.jsonObjektas.recipeTitle,
                             });
                         },
                         error: function (jq, status, message) {
-                            var msg = '$.ajax post error when calling http://tomasra.com:5000/match : ' + status + ' - Message: ' + message;
+                            var msg = '$.ajax post error when calling ' + MATCH_URL + '; status: ' + status + '; message: ' + message;
                             console.error(msg);
                             alert(msg);
                         }
                     });
-                //Isvalyti lentele is popup.html, updatinti kad ten rodytu loading...
-                //http://tomasra.com:5000/match
                 }
             });
         });
@@ -63,35 +55,34 @@ function checkWebpage(loadingStatus) {
     else if (loadingStatus == "onActivated") {
         chrome.storage.local.get("ingredientuSarasas", function (data) {
             console.log("ingredientuSarasas!!");
-            
+
             console.log(data);
 
-                chrome.tabs.query({ active: true }, function (tab) {
-                    tab = tab[0];
+            chrome.tabs.query({ active: true }, function (tab) {
+                tab = tab[0];
                 var fullUrl = tab.url;
-                
+
                 var recipeSiteExtractor = GetRecipeExtractor(fullUrl);
                 var arReceptoLangas = false;
                 if (recipeSiteExtractor) {
                     arReceptoLangas = true;
                 }
-                   console.log("arReceptoLangas: " + arReceptoLangas);
-                    var arYraIngredientuSarasas = data && data.ingredientuSarasas;
-                    console.log("arYraIngredientuSarasas: " + arYraIngredientuSarasas);
+                console.log("arReceptoLangas: " + arReceptoLangas);
+                var arYraIngredientuSarasas = data && data.ingredientuSarasas;
+                console.log("arYraIngredientuSarasas: " + arYraIngredientuSarasas);
 
-                    if (arReceptoLangas && arYraIngredientuSarasas) {
-                        console.log('recipe found');
-                        chrome.storage.local.set({ recipeFound: true });
-                        chrome.browserAction.setBadgeText({ text: "!" });
-                        chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-active.png" });
-                    } else {
-                        console.log('recipe not found');
-                        chrome.storage.local.set({ recipeFound: false });
-                        chrome.browserAction.setBadgeText({ text: "" });
-                        chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-neutral.png" });
-                    }
-                });
-            //}
+                if (arReceptoLangas && arYraIngredientuSarasas) {
+                    console.log('recipe found');
+                    chrome.storage.local.set({ recipeFound: true });
+                    chrome.browserAction.setBadgeText({ text: "!" });
+                    chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-active.png" });
+                } else {
+                    console.log('recipe not found');
+                    chrome.storage.local.set({ recipeFound: false });
+                    chrome.browserAction.setBadgeText({ text: "" });
+                    chrome.browserAction.setIcon({ path: "graphics/receptumeistras-icon-chrome-38-2-neutral.png" });
+                }
+            });
         });
     }
 };
@@ -99,10 +90,9 @@ function checkWebpage(loadingStatus) {
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     console.log("onActivated loadint..");
-    chrome.tabs.get(activeInfo.tabId, function(tab) {
-        // console.log(tab.url);
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
         var keyRecipeFound = 'recipe-found-' + tab.url;
-        chrome.storage.local.get(keyRecipeFound, function(data) {
+        chrome.storage.local.get(keyRecipeFound, function (data) {
             var recipeFound = data[keyRecipeFound];
             if (recipeFound === true) {
                 chrome.browserAction.setBadgeText({ text: "!" });
--- chrome_plugin/scripts/recipe_strategies.js
+++ chrome_plugin/scripts/recipe_strategies.js
@@ -8,23 +8,21 @@ var TukstReceptuStrategy = function () {
     this.getName = function () {
         return "1000receptu";
     };
-    
+
     this._getJsonFromRecipyTable = function (receptu_langas, url) {
         var jsonObjektas = {};
         jsonObjektas.recipeTitle = $('h1#article-title').text();
         jsonObjektas.url = url;
         jsonObjektas.ingredients = [];
-        
+
         $(receptu_langas).each(function (i, item) {
-            //console.log(i);
-            //console.log(item);
             var ingredientas = {};
             ingredientas.amount = $(item).find(".amount").html();
             ingredientas.ingredient = $(item).find(".ingredient a").html();
             // Special cases with ingredient lists consisting of multiple parts
             // http://www.delfi.lt/1000receptu/receptai/apsilaizysite-pirstelius-chacapuri-su-vistiena.d?id=71449664
-            if ((ingredientas.amount !== undefined && ingredientas.amount.trim().length > 0) 
-        || (ingredientas.ingredient !== undefined && ingredientas.ingredient.trim().length > 0)) {
+            if ((ingredientas.amount !== undefined && ingredientas.amount.trim().length > 0)
+                || (ingredientas.ingredient !== undefined && ingredientas.ingredient.trim().length > 0)) {
                 var urlWithGeneralName = $(item).find(".ingredient a").attr("href");
                 var urls = urlWithGeneralName.split("/");
                 var generalName = urls.pop();
@@ -46,13 +44,13 @@ var GaspadineStrategy = function () {
     this.getName = function () {
         return "Gaspadine";
     };
-    
+
     this._getJsonFromRecipyTable = function (receptu_langas, url) {
         var jsonObjektas = {};
         jsonObjektas.recipeTitle = $("div.article.recipe div h1[itemprop='name']").text();
         jsonObjektas.url = url;
         jsonObjektas.ingredients = [];
-        
+
         $(receptu_langas).each(function (i, item) {
             console.log(i);
             console.log(item);
@@ -77,13 +75,13 @@ var GaspadineStrategy = function () {
     }
 };
 
-var RECEPTU_PSL_URL_PREFIX_STRATEGIES = [
+const RECEPTU_PSL_URL_PREFIX_STRATEGIES = [
     {
-        url: "http://www.delfi.lt/1000receptu/receptai/",
+        url: "www.delfi.lt/1000receptu/receptai/",
         strategy: TukstReceptuStrategy
     },
     {
-        url: "http://www.gaspadine.lt/receptas/",
+        url: "www.gaspadine.lt/receptas/",
         strategy: GaspadineStrategy
     }
 ];
@@ -96,27 +94,27 @@ RecipeSiteExtractor.prototype = {
     setStrategy: function (recipeSiteStrategy) {
         this._recipeSiteStrategy = new recipeSiteStrategy;
     },
-    
+
     getName: function () {
         return this._recipeSiteStrategy.getName();
     },
-    
+
     extractRecipeData: function () {
         return this._recipeSiteStrategy.extractRecipeData();
     }
 };
 
 
-var GetRecipeExtractor = function (fullUrl){
-    var recipeSiteExtractor = new RecipeSiteExtractor();
-    var yraReceptuPuslapis = false;
-    var i = 0;
+const GetRecipeExtractor = function (fullUrl) {
+    const urlWithoutHttp = fullUrl.replace(/(http|https):\/\/(\S+)/g, `\$2`);
+
+    const recipeSiteExtractor = new RecipeSiteExtractor();
+    let yraReceptuPuslapis = false;
+    let i = 0;
     while (yraReceptuPuslapis === false && i < RECEPTU_PSL_URL_PREFIX_STRATEGIES.length) {
-        console.log(i);
-        if (fullUrl.substring(0, RECEPTU_PSL_URL_PREFIX_STRATEGIES[i].url.length) === RECEPTU_PSL_URL_PREFIX_STRATEGIES[i].url) {
+        if (urlWithoutHttp.substring(0, RECEPTU_PSL_URL_PREFIX_STRATEGIES[i].url.length) === RECEPTU_PSL_URL_PREFIX_STRATEGIES[i].url) {
             yraReceptuPuslapis = true;
             recipeSiteExtractor.setStrategy(RECEPTU_PSL_URL_PREFIX_STRATEGIES[i].strategy);
-            console.log(recipeSiteExtractor.getName());
             return recipeSiteExtractor;
         }
         i++;
